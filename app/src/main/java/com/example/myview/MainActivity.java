5a176d4e6348ecc2daf541a97222ed299d7113f2
陈创智 <chenchuangzhi@meizu.com>
2022-07-19 15:12:59 +0800

    ViewPager 2

--- app/src/main/java/com/example/myview/MainActivity.java
+++ app/src/main/java/com/example/myview/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);////
+        setContentView(R.layout.activity_main);
+
+        Button shuai = (Button) this.findViewById(R.id.shuai);
+        shuai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,ScreenSlidePagerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/myview/ScreenSlidePageFragment.java
@@ -0,0 +1,20 @@
+package com.example.myview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+    }
+}
